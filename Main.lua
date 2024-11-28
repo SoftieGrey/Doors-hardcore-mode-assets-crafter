5a0b3b1012dfc648296bfa22ae7678749514b2d6
softbf395 <130502864+softbf395@users.noreply.github.com>
2024-11-28 09:34:06 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,14 @@
 local utility=loadstring(game:HttpGet("https://raw.githubusercontent.com/SoftieGrey/Doors-hardcore-mode-assets-crafter/refs/heads/main/ExampleSpawner.lua"))()
 function quick()
+  if not isfile("quick.png")
+-- local Attach=Instance.new("Attachment")
+-- local AudioListFolder=Instance.new("Folder")
+-- local Audio1=Instance.new("Sound", AudioListFolder)
+-- Audio1.SoundId="rbxassetid://0"
+-- Audio1.Name="Spawn"
+-- Audio1.Volume=3
+-- local Audio2=Instance.new("Sound", AudioListFolder)
+-- Audio2.SoundId="rbxassetid://0"
+-- Audio2.Name="Footsteps"
+-- Audio2.Volume=3
+-- utility:Spawn(Attach, AudioListFolder, "Namw", 100, {2, 6}, false, "Guiding", false, "Guiding", {"You died to Name", "wait what?", "its called..", "Name!?"})
