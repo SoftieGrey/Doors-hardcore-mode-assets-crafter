6ad291b3ef993b33fafea527a3a6a9cfe8660130
softbf395 <130502864+softbf395@users.noreply.github.com>
2024-11-28 12:41:40 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 local utility=loadstring(game:HttpGet("https://raw.githubusercontent.com/SoftieGrey/Doors-hardcore-mode-assets-crafter/refs/heads/main/ExampleSpawner.lua"))()
 function download(url)
-  local i=url:split("/")[9]
+  local path=url:split("/")[9]
   if not isfile(path) then
     writefile(path, url)
   end
@@ -18,12 +18,40 @@ function quick()
   ParticleEmitter.SpreadAngle = Vector2.new(180, 180) -- `SpreadAngle` must be a Vector2.
    local AudioListFolder=Instance.new("Folder")
    local Audio1=Instance.new("Sound", AudioListFolder)
-   Audio1.SoundId="rbxassetid://0"
+   Audio1.SoundId="rbxassetid://8654282340"
    Audio1.Name="Spawn"
    Audio1.Volume=3
    local Audio2=Instance.new("Sound", AudioListFolder)
-   Audio2.SoundId="rbxassetid://0"
+   Audio2.SoundId="rbxassetid://8654282340"
    Audio2.Name="Footsteps"
    Audio2.Volume=3
-  utility:Spawn(Attach, AudioListFolder, "Quick", 100, {1,2}, false, "Guiding", false, "Guiding", {"You died to Name", "wait what?", "its called..", "Name!?"})
+  utility:Spawn(
+        1.5, --speed
+        Attach, --attachment for custom particles
+        AudioListFolder, --audios
+        "Quick", --entity name
+        100, --damage
+        {1, 2}, --rebounds ({0,0} means no rebounds, nil is the same)
+        false, --eathquakes
+        "Guiding", --Crucifix Type ("Guiding" or "Curious")
+        false, --Crucifix fails?
+        "Guiding", --Death type ("Curious" or "Guiding")
+        {"You died to who you call Quick", "Listen for any cues that might hint it's arrival", "Find a hiding spot quickly! It's speed has been proved to be quite menacing", ""} --death messages
+        jumpscare=function()
+        
+    end
+    )
+end
+local entityChances={
+  Quick={Door=10, Chance=35}
+}
+local canEntitiesSpawn=game.ReplicatedStorafe.ChaseInSession
+local roomsBlacklist={50,99,100}
+function canEntitySpawn()
+  local room=game.ReplicatedStorage.LatestRoom.Value
+  if canEntitiesSpawn.Value==false and not table.find(roomsBlacklist, room) then
+    return true
+  else
+    return false
+  end
 end
